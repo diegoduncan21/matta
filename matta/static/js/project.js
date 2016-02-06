f9abc1ea35ef38977e6d69bdb1c264ee6104e3a7
sapox <zapponi.juan.jose@gmail.com>
2016-02-06 04:34:51 -0300

    files for scene and hope last version of revolucion

--- matta/static/js/project.js
+++ matta/static/js/project.js
@@ -1,1 +1,16 @@
 /* Project specific Javascript goes here. */
+$(document).ready(function() {
+    
+    $('#play').click(function(){
+        if ($('#play').attr('src') == '/static/images/sound-specs.gif') {
+            $('#audio').trigger('pause');
+            $('#play').attr('src','/static/images/glyphicons-174-play.png');
+            console.log("pausa");
+        } else {
+            $('#audio').trigger('play');
+            $('#play').attr('src','/static/images/sound-specs.gif');
+            console.log("play");
+        }
+    });
+
+});
