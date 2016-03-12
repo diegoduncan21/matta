49bafce596ccdfde51c2363e48ef26138813a637
sapox <zapponi.juan.jose@gmail.com>
2016-03-12 13:58:05 -0300

    fix the icon behavior

--- matta/static/js/project.js
+++ matta/static/js/project.js
@@ -1,14 +1,14 @@
 /* Project specific Javascript goes here. */
 $(document).ready(function() {
-    
+
     $('#play').click(function(){
-        if ($('#play').attr('src') == '/static/images/sound-specs.gif') {
+        if ($('#play').attr('src') == '/static/images/soundspect_.gif') {
             $('#audio').trigger('pause');
             $('#play').attr('src','/static/images/glyphicons-174-play.png');
             console.log("pausa");
         } else {
             $('#audio').trigger('play');
-            $('#play').attr('src','/static/images/sound-specs.gif');
+            $('#play').attr('src','/static/images/soundspect_.gif');
             console.log("play");
         }
     });
