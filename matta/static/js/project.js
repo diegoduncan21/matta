aef432b1ac75fc69da7084f82b34b3b686cd03a0
sapox <zapponi.juan.jose@gmail.com>
2016-03-13 23:08:12 -0300

    new image background and footer

--- matta/static/js/project.js
+++ matta/static/js/project.js
@@ -5,10 +5,18 @@ $(document).ready(function() {
         if ($('#play').attr('src') == '/static/images/soundspect_.gif') {
             $('#audio').trigger('pause');
             $('#play').attr('src','/static/images/glyphicons-174-play.png');
+            $('#music').css({
+              'margin-left':'13px',
+              'bottom': '3px'
+            });
             console.log("pausa");
         } else {
             $('#audio').trigger('play');
             $('#play').attr('src','/static/images/soundspect_.gif');
+            $('#music').css({
+              'margin-left':'0px',
+              'bottom': '36px'
+            });
             console.log("play");
         }
     });
